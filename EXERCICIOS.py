a928375b2ed6d4de14512017750884720070fd2d
Theuzin <mateusetec424@gmail.com>
2025-04-26 16:19:05 -0300

    Update EXERCICIOS.py

--- EXERCICIOS.py
+++ EXERCICIOS.py
@@ -294,7 +294,7 @@ else:
 
 #DESAFIO 029 DO CURSO EM VÍDEO
 
-elocidade = float(input('Qual a velocidade do carro? '))
+velocidade = float(input('Qual a velocidade do carro? '))
 
 if velocidade > 80:
     print('Você será multado por R${:.2f}, por estar {:.0f}Km/h acima dos 80KM/h'.format((velocidade - 80) * 7, velocidade - 80))
