02e233d8a17ccaa64fe6c2f9a73ca2ed444ae2bb
Theuzin <mateusetec424@gmail.com>
2025-04-25 22:12:50 -0300

    Update EXERCICIOS.py

--- EXERCICIOS.py
+++ EXERCICIOS.py
@@ -1,1 +1,134 @@
+#AVISO CARO DOCENTE DO CURSO EM VÍDEO, ESSA SÃO APENAS RESOLUÇÕES COM BASE EM MINHA VISAO, TENTA NÃO COPIAR E FAZER O SEU PRÓPRIO
+#LEMBRANDO QUE É MUITO IMPORTANTE À PRATICA NO SEU APRENDIZADO, ANTES DE VER CÓDIGO PRONTO
+#ESSE REPOSITÓRIO E PARA FINS EDUCATIVOS, PARA CORREÇÕES OU NÃO SOUBER COMO FAZER SUA PRÓPRIA RESOLUÇÃO
+#SITE DO CURSO DE PHYTON www.cursoemvideo.com
+#CURSO APRENSENTADO POR GUSTAVO GUANABARA
+#STAY FOCUSED STUDENT, PRACTICE AND STUDY WILL HELP YOUR LEARNING.
 
+
+#DESAFIO 001 DO CURSO EM VÍDEO PHYTON
+
+  #Primero Formato
+print('Olá, Mundo!')
+
+  #Segundo Formato
+mensagem = 'Olá, Mundo!'
+print(mensagem)
+
+#DESAFIO 002 DO CURSO EM VÍDEO PHYTON
+
+nome = str(input('Digite seu nome: '))
+print('é um Prazer em conhecer-lo {}'.format(nome))
+
+#DESAFIO 003 DO CURSO EM VÍDEO PHYTON
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor:: '))
+print('A soma entre {} e {} é igual a {}'.format(num1, num2, num1+num2))
+
+#DESAFIO 005 DO CURSO EM VÍDEO PHYTON
+
+nome = input('Digite algo: ')
+print('O tipo primitivo é: ', type(nome))
+print('So tem espaços? {}'.format(nome.isspace()))
+print('é um número? {}'.format(nome.isnumeric()))
+print('É analfabético? {}'.format(nome.isalpha()))
+print('É alfanumérico? {}'.format(nome.isalnum()))
+print('Está em MAIUSCULO? {}'.format(nome.isupper()))
+print('Está em minusculo? {}'.format(nome.islower()))
+print('Está capitalizada? {}'.format(nome.istitle()))
+
+#DESAFIO 005 DO CURSO EM VÍDEO PHYTON
+
+numero = int(input('Digite um valor: '))
+print('Analisando o valor {}, seu antecessor é {}, e seu sucessor {}'.format(numero, numero - 1, numero + 1))
+
+#DESAFIO 006 DO CURSO EM VÍDEO PHYTON
+
+#Primeira Forma utilizando a importação MATH, disponibilizando varias formulas. Mas eu utilizei o SQRT(Raiz quadrada)
+import math
+
+numero = int(input('Digite um valor: '))
+print('Analisando o valor {} \nseu dobro é {} \nseu triplo é {} \nsua raiz quadrada é {:.1f}'.format(numero, numero * 2, numero * 3, math.sqrt(numero)))
+
+#Segunda Forma
+numero = int(input('Digite um valor: '))
+print('Analisando o valor {} \nseu dobro é {} \nseu triplo é {} \n sua raiz quadrada é {:.1f}'.format(numero, numero * 2, numero * 3, numero ** (1/2)))
+
+#DESAFIO 007 DO CURSO EM VÍDEO PHYTON
+
+n1 = float(input('Nota da primeira prova: '))
+n2 = float(input('Nota da segunda prova: '))
+print('A média entre {} e {} é igual {:.1f}'.format(n1, n2, (n1 + n2) / 2))
+
+#DESAFIO 008 DO CURSO EM VÍDEO PHYTON
+
+metros = float(input('Distância em metros: '))
+print('A medida {} corresponde a'.format(metros))
+print('{}KM'.format(metros / 1000))
+print('{}HM'.format(metros / 100))
+print('{}dam'.format(metros / 10))
+print('{:.1f}DM'.format(metros * 10))
+print('{:.1f}CM'.format(metros * 100))
+print('{:.1f}MM'.format(metros * 1000))
+
+#DESAFIO 009 DO CURSO EM VÍDEO PHYTON
+
+
+#Primeiro Forma, mas haverá muitas linhas de código
+
+tabuada = int(input('Digite um numero para sua tabuada: '))
+print('-' * 20)
+print('{} X 1 = {}'.format(tabuada, tabuada  * 1))
+print('{} X 2 = {}'.format(tabuada, tabuada * 2))
+print('{} X 3 = {}'.format(tabuada, tabuada * 3))
+print('{} X 4 = {}'.format(tabuada, tabuada * 4))
+print('{} X 5 = {}'.format(tabuada, tabuada * 5))
+print('{} X 6 = {}'.format(tabuada, tabuada * 6))
+print('{} X 7 = {}'.format(tabuada, tabuada * 7))
+print('{} X 8 = {}'.format(tabuada, tabuada * 8))
+print('{} X 9 = {}'.format(tabuada, tabuada * 9))
+print('{} X 10 = {}'.format(tabuada, tabuada * 10))
+print('-' * 20)
+
+#Segunda Forma, mas a linha codígo para executar é extensa
+
+tabuada = int(input('Digite um numero para a sua tabuada: '))
+print('A tabuada do seu numero é: {} {} {} {} {} {} {} {} {} {}'.format(tabuada * 1, tabuada * 2, tabuada * 3, tabuada * 4, tabuada * 5, tabuada * 6, tabuada * 7, tabuada * 8, tabuada * 9, tabuada * 10))
+
+#DESAFIO 010 DO CURSO EM VÍDEO PHYTON
+
+carteira = float(input('Quanto dinheiro você tem na carteira?: '))
+print('Com R${:.2f} você pode comprar U${:.2f}'.format(carteira, carteira / 5.69))
+print('Com R${:.2f} você pode comprar Euro${:.2f}'.format(carteira, carteira / 6.49))
+
+#DESAFIO 011 DO CURSO EM VÍDEO PHYTON
+
+largura = float(input('Largura: '))
+altura = float(input('Altura: '))
+print('Sua parede tem dimensão {}x{} e sua área é de {}m(^2)'.format(largura, altura, largura * altura))
+print('Para pintar essa parede, você precisará de {}l de tinta.'.format(largura * altura / 2))
+
+#DEASFIO 012 DO CURSO EM VÍDEO PHYTON
+
+
+  #Primeira Forma mais direto
+preco = float(input('Qual é o preço do produto? '))
+print('O produto que custava R${:.2f}, na promoção com desconto 5% vai custar R${:.2f}'.format(preco, preco - (preco * 5 / 100)))
+
+  #Segunda Forma com a variavel desconto
+preco = float(input('Qual é o preço do produto? '))
+desconto = preco - (preco * 5 / 100)
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}'.format(preco, desconto))
+
+#DESAFIO 013 DO CURSO EM VÍDEO PHYTON
+
+
+  #Primeira forma mais direto
+salario = float(input('Digite o salário do funcionário? '))
+print('Um funcionário que ganhava R${:.2f}, com aumento de 15%, passa a receber R${:.2f}'.format(salario, salario + (salario * 15 / 100)))
+
+  #Segunda Forma com variavel aumento
+salario = float(input('Digite o salário do funcionário? '))
+aumento = salario + (salario * 15 / 100)
+print('Um funcionário que ganhava R${:.2f}, com aumento de 15%, passa a receber R${:.2f}'.format(salario, aumento))
