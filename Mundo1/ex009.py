e675a019da067863b01fb453ee3bd7eaa1e69095
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:18:44 -0300

    Update ex009.py

--- Mundo1/ex009.py
+++ Mundo1/ex009.py
@@ -1,1 +1,23 @@
+#DESAFIO 009 DO CURSO EM VÍDEO PHYTON
 
+
+#Primeiro Forma, mas haverá muitas linhas de código
+
+tabuada = int(input('Digite um numero para sua tabuada: '))
+print('-' * 20)
+print('{} X 1 = {}'.format(tabuada, tabuada  * 1))
+print('{} X 2 = {}'.format(tabuada, tabuada * 2))
+print('{} X 3 = {}'.format(tabuada, tabuada * 3))
+print('{} X 4 = {}'.format(tabuada, tabuada * 4))
+print('{} X 5 = {}'.format(tabuada, tabuada * 5))
+print('{} X 6 = {}'.format(tabuada, tabuada * 6))
+print('{} X 7 = {}'.format(tabuada, tabuada * 7))
+print('{} X 8 = {}'.format(tabuada, tabuada * 8))
+print('{} X 9 = {}'.format(tabuada, tabuada * 9))
+print('{} X 10 = {}'.format(tabuada, tabuada * 10))
+print('-' * 20)
+
+#Segunda Forma, mas a linha codígo para executar é extensa
+
+tabuada = int(input('Digite um numero para a sua tabuada: '))
+print('A tabuada do seu numero é: {} {} {} {} {} {} {} {} {} {}'.format(tabuada * 1, tabuada * 2, tabuada * 3, tabuada * 4, tabuada * 5, tabuada * 6, tabuada * 7, tabuada * 8, tabuada * 9, tabuada * 10))
