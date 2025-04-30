36b90253d906be249a42cade0ea7a494633f0b44
Theuzin <mateusetec424@gmail.com>
2025-04-30 20:10:43 -0300

    Create ex016.py

---
+++ Mundo1/ex016.py
@@ -0,0 +1,5 @@
+#DESAFIO 016 DO CURSO EM VÍDEO PHYTON
+
+from math import floor
+numero = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(numero, floor(numero)))
