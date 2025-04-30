fcb20b6d5de50c0bbb5f8244f603bf6c2145775d
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:18:23 -0300

    Update ex008.py

--- Mundo1/ex008.py
+++ Mundo1/ex008.py
@@ -1,1 +1,10 @@
+#DESAFIO 008 DO CURSO EM VÍDEO PHYTON
 
+metros = float(input('Distância em metros: '))
+print('A medida {} corresponde a'.format(metros))
+print('{}KM'.format(metros / 1000))
+print('{}HM'.format(metros / 100))
+print('{}dam'.format(metros / 10))
+print('{:.1f}DM'.format(metros * 10))
+print('{:.1f}CM'.format(metros * 100))
+print('{:.1f}MM'.format(metros * 1000))
