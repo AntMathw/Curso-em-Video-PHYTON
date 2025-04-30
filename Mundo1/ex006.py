7bc355173601217ce08677b720348a823e3ae0ab
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:17:51 -0300

    Update ex006.py

--- Mundo1/ex006.py
+++ Mundo1/ex006.py
@@ -1,1 +1,11 @@
+#DESAFIO 006 DO CURSO EM VÍDEO PHYTON
 
+#Primeira Forma utilizando a importação MATH, disponibilizando varias formulas. Mas eu utilizei o SQRT(Raiz quadrada)
+import math
+
+numero = int(input('Digite um valor: '))
+print('Analisando o valor {} \nseu dobro é {} \nseu triplo é {} \nsua raiz quadrada é {:.1f}'.format(numero, numero * 2, numero * 3, math.sqrt(numero)))
+
+#Segunda Forma
+numero = int(input('Digite um valor: '))
+print('Analisando o valor {} \nseu dobro é {} \nseu triplo é {} \n sua raiz quadrada é {:.1f}'.format(numero, numero * 2, numero * 3, numero ** (1/2)))
