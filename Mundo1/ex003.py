919f2cc5877d0b2e116cf3e0b7bec54e181a2119
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:13:27 -0300

    Update ex003.py

--- Mundo1/ex003.py
+++ Mundo1/ex003.py
@@ -1,1 +1,5 @@
+#DESAFIO 003 DO CURSO EM VÍDEO PHYTON
 
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor:: '))
+print('A soma entre {} e {} é igual a {}'.format(num1, num2, num1+num2))
