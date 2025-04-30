18e3fa3f7075e896b3e934cca12af4699e5c883d
Theuzin <mateusetec424@gmail.com>
2025-04-30 20:09:47 -0300

    Update ex013.py

--- Mundo1/ex013.py
+++ Mundo1/ex013.py
@@ -1,1 +1,12 @@
+#DESAFIO 013 DO CURSO EM VÍDEO PHYTON
+
+
+  #Primeira forma mais direto
+salario = float(input('Digite o salário do funcionário? '))
+print('Um funcionário que ganhava R${:.2f}, com aumento de 15%, passa a receber R${:.2f}'.format(salario, salario + (salario * 15 / 100)))
+
+  #Segunda Forma com variavel aumento
+salario = float(input('Digite o salário do funcionário? '))
+aumento = salario + (salario * 15 / 100)
+print('Um funcionário que ganhava R${:.2f}, com aumento de 15%, passa a receber R${:.2f}'.format(salario, aumento))
 
