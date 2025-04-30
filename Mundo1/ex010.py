464b983bdb3a57ddf80fc9680a28513b8dee3e60
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:19:00 -0300

    Update ex010.py

--- Mundo1/ex010.py
+++ Mundo1/ex010.py
@@ -1,1 +1,5 @@
+#DESAFIO 010 DO CURSO EM VÍDEO PHYTON
 
+carteira = float(input('Quanto dinheiro você tem na carteira?: '))
+print('Com R${:.2f} você pode comprar U${:.2f}'.format(carteira, carteira / 5.69))
+print('Com R${:.2f} você pode comprar Euro${:.2f}'.format(carteira, carteira / 6.49))
