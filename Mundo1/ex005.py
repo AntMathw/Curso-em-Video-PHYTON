aa42b391516cd88fcef3481ec87f9981183344ce
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:16:37 -0300

    Update ex005.py

--- Mundo1/ex005.py
+++ Mundo1/ex005.py
@@ -1,1 +1,5 @@
+#DESAFIO 005 DO CURSO EM VÍDEO PHYTON
+
+numero = int(input('Digite um valor: '))
+print('Analisando o valor {}, seu antecessor é {}, e seu sucessor {}'.format(numero, numero - 1, numero + 1))
 
