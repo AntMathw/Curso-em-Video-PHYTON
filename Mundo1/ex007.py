777d98f2e9a1ef8cc7ad97ebfa5536308e9c71dc
Theuzin <mateusetec424@gmail.com>
2025-04-30 11:18:05 -0300

    Update ex007.py

--- Mundo1/ex007.py
+++ Mundo1/ex007.py
@@ -1,1 +1,5 @@
+#DESAFIO 007 DO CURSO EM VÍDEO PHYTON
 
+n1 = float(input('Nota da primeira prova: '))
+n2 = float(input('Nota da segunda prova: '))
+print('A média entre {} e {} é igual {:.1f}'.format(n1, n2, (n1 + n2) / 2))
