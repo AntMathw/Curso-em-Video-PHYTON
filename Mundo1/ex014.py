fa0e3bae382a259f4b5ed67be15d037e01ef5035
Theuzin <mateusetec424@gmail.com>
2025-04-30 20:10:10 -0300

    Create ex014.py

---
+++ Mundo1/ex014.py
@@ -0,0 +1,10 @@
+#DESAFIO 014 DO CURSO EM VÍDEO PHYTON
+
+#Primeira Forma
+c = float(input('Informa a temperatura em Celsius: '))
+F = (c * 1.8 + 32)
+print('A temperatura de {} Celsius, em fahrenheit é {}'.format(c, F))
+
+#Segunda Forma
+c = float(input('Informa a temperatura em Celsius: '))
+print('A temperatura de {} Celsius, em fahrenheint é {}'.format(c, c * 1.8 + 32))
