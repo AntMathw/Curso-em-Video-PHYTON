41c0569e5bf5275e7878f2e1a2e6e6717aeb019f
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:59:24 -0300

    Update ex039.py

--- Mundo2/ex039.py
+++ Mundo2/ex039.py
@@ -1,1 +1,14 @@
+#DESAFIO 039 DO CURSO EM VÍDEO
+
+from datetime import date
+
+nascimento = int(input('Em que ano você nasceu? '))
+hoje = date.today().year
+
+if nascimento == hoje:
+    print('O ano de {}, você obrigatoriamente tem que se alistar! '.format(date.today().year))
+elif hoje - nascimento > 18:
+    print('O ano de {}, alistamento ja ocorreu, mas se não se alistou, FAÇA!'.format(nascimento + 18))
+else:
+    print('O seu alistamento ainda não chegou. Ele vai acontecer em {}'.format(nascimento + 18))
 
