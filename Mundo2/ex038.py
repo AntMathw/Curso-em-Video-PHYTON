cb6bc3d658ad9ee806dbaa51d39b225ccd233e60
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:59:12 -0300

    Update ex038.py

--- Mundo2/ex038.py
+++ Mundo2/ex038.py
@@ -1,1 +1,8 @@
+#DESAFIO 038 DO CURSO EM VÍDEO
 
+num1 = int(input('Digite primero valor: '))
+num2 = int(input('Digite segundo valo: '))
+if num1 > num2:
+    print('O primeiro valor é maior')
+elif num2 > num1:
+    print('O segundo valor é maior')
