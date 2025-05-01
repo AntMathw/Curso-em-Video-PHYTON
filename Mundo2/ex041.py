ebb576e43ae4538d3c9c846fe8bf79b1f3f487d4
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:59:52 -0300

    Update ex041.py

--- Mundo2/ex041.py
+++ Mundo2/ex041.py
@@ -1,1 +1,18 @@
+#DESAFIO 041 DO CURSO EM VÍDEO
 
+from datetime import date
+
+idade = int(input('Digite sua idade ou ano de nascimento: '))
+anoatual = date.today().year
+subtrai = anoatual - idade
+
+if idade <= 9 or subtrai <= 9:
+    print('Categoria será MIRIM.')
+elif idade <= 14 or subtrai <= 14:
+    print('Cateoria será INFANTIL.')
+elif idade <= 19 or subtrai <= 19:
+    print('Categoria será JÚNIOR.')
+elif idade <= 25 or subtrai <= 25:
+    print('Categoria será SÊNIOR.')
+elif idade > 25:
+    print('Categoria será MASTER.')
