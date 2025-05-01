9c09976e57d5be499a51a169d88114bcab2f32fa
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:49:08 -0300

    Update ex022.py

--- Mundo1/ex022.py
+++ Mundo1/ex022.py
@@ -1,1 +1,9 @@
+#DESAFIO 022 DO CURSO EM VÍDEO PHYTON
+
+nome = str(input('Digite seu nome completo: '))
+print('Analisando seu nome...')
+print('Seu nome em maiusculo é {}'.format(nome.upper()))
+print('Seu nome em minusculo é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome {} tem {} letras'.format(nome.split()[0], len(nome.split()[0])))
 
