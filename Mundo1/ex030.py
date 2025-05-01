2237634f18abbd974fe3496b1afafbe5bf45e2c9
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:53:52 -0300

    Update ex030.py

--- Mundo1/ex030.py
+++ Mundo1/ex030.py
@@ -1,1 +1,9 @@
+#DESAFIO 030 DO CURSO EM VÍDEO
+
+numero = int(input('Digite um numero qualquer: '))
+
+if numero % 2 == 0:
+    print('Esse numero {} é PAR'.format(numero))
+else:
+    print('Esse número {} é IMPAR'.format(numero))
 
