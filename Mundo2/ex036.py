60b4db9311fb415184b298426936183e481f2682
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:58:49 -0300

    Update ex036.py

--- Mundo2/ex036.py
+++ Mundo2/ex036.py
@@ -1,1 +1,13 @@
+#DESAFIO 036 DO CURSO EM VÍDEO
 
+casa = float(input('Qual é o valor da casa? '))
+salario = float(input('Salário do comprador?  '))
+ano = int(input('Quantos anos ele vai pagar? '))
+emprestimo = casa / (ano * 12)
+pmensal = emprestimo * 100 / salario
+print('Para pagar uma casa de R${} em {:.0f} anos, a prestação será de R${:.2f}'.format(casa, ano, emprestimo, pmensal))
+
+if emprestimo <= 0.30 * salario:
+    print('\033[1;32mEmpréstimo CONCEDIDO!\033[m ')
+elif emprestimo > 0.30 * salario:
+    print('\033[1;31mEmpréstimo NEGADO!\033[m ')
