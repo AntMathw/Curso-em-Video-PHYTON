43e0409cb66b93261cbbe5ad49fbea36c3c6ad98
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:59:37 -0300

    Update ex040.py

--- Mundo2/ex040.py
+++ Mundo2/ex040.py
@@ -1,1 +1,12 @@
+#DESAFIO 040 DO CURSO EM VÍDEO
 
+prova1 = float(input('Nota da primeia prova: '))
+prova2 = float(input('Nota da segunda prova: '))
+resultado = (prova1 + prova2) / 2
+
+if resultado >= 7:
+    print('A nota {} e {}, a média do aluno vai ser {}\n\033[1;36mAPROVADO\033[m'.format(prova1, prova2, resultado))
+elif resultado < 5.0:
+    print('A nota {} e {}, a média do aluno vai ser {}\n\033[1;31mREPROVADO\033[m'.format(prova1, prova2, resultado))
+elif resultado == 5.9 or 6.9:
+    print('A nota {} e {}, a média do aluno vai ser {}\nRECUPERAÇÃO'.format(prova1, prova2, (resultado)))
