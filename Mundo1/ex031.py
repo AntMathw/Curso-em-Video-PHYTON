d2f02f75269a83232c5c9db7044de2e43379c3a3
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:54:07 -0300

    Update ex031.py

--- Mundo1/ex031.py
+++ Mundo1/ex031.py
@@ -1,1 +1,9 @@
+#DESAFIO 031 DO CURSO EM VÍDEO
 
+viagem = float(input('Qual a distância da viagem? '))
+print('Você está preste a fazer uma viagem de {}KM'.format(viagem))
+
+if viagem  <= 200:
+    print('O preço da sua passagem será de R${:.2f}'.format(viagem * 0.50))
+else:
+    print('O preço da sua passagem será de R${:.2f}'.format(viagem * 0.45))
