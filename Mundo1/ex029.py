fd5903db5c5fb9290247f27c41a7a5efa451d7d3
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:53:38 -0300

    Update ex029.py

--- Mundo1/ex029.py
+++ Mundo1/ex029.py
@@ -1,1 +1,9 @@
+#DESAFIO 029 DO CURSO EM VÍDEO
+
+velocidade = float(input('Qual a velocidade do carro? '))
+
+if velocidade > 80:
+    print('Você será multado por R${:.2f}, por estar {:.0f}Km/h acima dos 80KM/h'.format((velocidade - 80) * 7, velocidade - 80))
+else:
+    print('Tenha um bom dia! Diriga com Segurança')
 
