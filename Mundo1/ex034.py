9652ad1143eb92733f08c0a87194b9c9b3ac8083
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:54:57 -0300

    Update ex034.py

--- Mundo1/ex034.py
+++ Mundo1/ex034.py
@@ -1,1 +1,8 @@
+#DESAFIO 034 DO CURSO EM VÍDEO
 
+salario = float(input('Qual o valor do salário do funcionário? R$ '))
+
+if salario > 1250.00:
+    print('Quem ganhava R${}, passa a ganhar R${}'.format(salario, salario * 1.10))
+else:
+    print('Quem ganhava R${}, passa a ganhar R${}'.format(salario, salario * 1.15))
