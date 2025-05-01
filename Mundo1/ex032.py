2ad6fe62f319721b6df8465a0f7233a23db94883
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:54:21 -0300

    Update ex032.py

--- Mundo1/ex032.py
+++ Mundo1/ex032.py
@@ -1,1 +1,13 @@
+#DESAFIO 032 DO CURSO EM VÍDEO
 
+from datetime import date
+
+ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+
+if ano == 0:
+  ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano de {} é bissexto'.format(ano))
+else:
+    print('O ano de {} não é bissexto'.format(ano))
