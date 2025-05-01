859897591d1925a92e79d8c275b4d01a52a49f84
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:49:20 -0300

    Update ex023.py

--- Mundo1/ex023.py
+++ Mundo1/ex023.py
@@ -1,1 +1,8 @@
+#DESAFIO 023 DO CURSO EM VÍDEO
 
+numero = int(input('Informe um número: '))
+print('Analisando esse numero {}'.format(numero))
+print('Unidade: {}'.format(numero // 1 % 10))
+print('Dezena: {}'.format(numero // 10 % 10))
+print('Centena: {}'.format(numero // 100 % 10))
+print('Milhar: {}'.format(numero // 1000 % 10))
