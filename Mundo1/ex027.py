19c5d9765e82dfecf37a343134f57a058bbd42ad
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:53:10 -0300

    Update ex027.py

--- Mundo1/ex027.py
+++ Mundo1/ex027.py
@@ -1,1 +1,7 @@
+#DESAFIO 027 DO CURSO EM VÍDEO
 
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Prazer em te conhecer! ')
+dividido = nome.split()
+print('Seu primeiro nome é: {}'.format(dividido[0]))
+print('Seu ultimo nome é: {}'.format(dividido[-1]))
