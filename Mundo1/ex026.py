d420022f3ef89c053880ba89d62f909cf0616176
Theuzin <mateusetec424@gmail.com>
2025-05-01 12:52:57 -0300

    Update ex026.py

--- Mundo1/ex026.py
+++ Mundo1/ex026.py
@@ -1,1 +1,6 @@
+#DESAFIO 026 DO CURSO EM VÍDEO
 
+frase = str(input('Digite uma frase: ')).strip()
+print('A letra "A" aparece {} vezes na frase "{}".'.format(frase.lower().count('a'), frase))
+print('A letra "A" aparece pela primeira vez na posição {}'.format(frase.lower().find('a')+1))
+print('A letra "A" aparece pela última vez na posição {}'.format(frase.lower().rfind('a')+1))
