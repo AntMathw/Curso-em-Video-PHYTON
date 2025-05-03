3f4917fb36e8bde18ec7f16cc53963f404ff08e0
Theuzin <mateusetec424@gmail.com>
2025-05-03 13:36:04 -0300

    Update ex051.py

--- Mundo2/ex051.py
+++ Mundo2/ex051.py
@@ -1,1 +1,12 @@
+#DESAFIO 051 DO CURSO EM VÍDEO PHYTON
 
+print('=' * 35)
+print('TERMOS DE UMA PROGRESSÃO ARITMÉTICA')
+print('=' * 35)
+
+primeiro = int(input('Digite o pimeiro termo: '))
+razao = int(input('Digite a razão: '))
+
+for i in range(0 , 10):
+    print(primeiro + (razao * i), end=' ')
+print('FIM')
