c4801997583b1a83af449c242afa64c5014fe079
Theuzin <mateusetec424@gmail.com>
2025-05-03 13:08:12 -0300

    Update ex048.py

--- Mundo2/ex048.py
+++ Mundo2/ex048.py
@@ -1,1 +1,11 @@
+#DESAFIO 048 DO CURSO EM VÍDEO PHYTON
+
+cont = 0
+soma = 0
+for impar in range(3, 501, 3):
+    if impar % 2 == 1:
+        cont += impar
+        soma += 1
+
+print('A soma é {} e o numero de valores é {}'.format(soma, cont, end=' '))
 
