dadef43c5e35a28617ed3700cea0b25183a504bc
Theuzin <mateusetec424@gmail.com>
2025-05-03 13:47:58 -0300

    Update ex052.py

--- Mundo2/ex052.py
+++ Mundo2/ex052.py
@@ -1,1 +1,17 @@
+#DESAFIO 052 DO CURSO EM VÍDEO PHYTON
 
+num = int(input('Digite um número: '))
+total = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        print('\033[34m', end='')
+        total += 1
+
+    else:
+        print('\033[m', end='')
+    print('{} '.format(i), end='')
+if total == 2:
+    print('\n\033[mÉ um número primo.')
+else:
+    print('\n\033[mEsté número não é primo.')
