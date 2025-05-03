a0c455db5c6b50505d7f5966f7b800616a109a09
Theuzin <mateusetec424@gmail.com>
2025-05-03 14:37:10 -0300

    Update ex053.py

--- Mundo2/ex053.py
+++ Mundo2/ex053.py
@@ -1,1 +1,12 @@
+#DEASFIO 053 DO CURSO EM VÍDEO PHYTON
 
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+j = ''.join(palavras)
+inv = ''
+for letra in range(len(j) - 1, - 1, - 1):
+    inv += j[letra]
+if inv == j:
+    print('É políndromo.')
+else:
+    print('Não é políndromo.')
