09bd9f9436432bd3ee51e0d40e7632c04a02b640
Theuzin <mateusetec424@gmail.com>
2025-05-03 13:14:12 -0300

    Update ex049.py

--- Mundo2/ex049.py
+++ Mundo2/ex049.py
@@ -1,1 +1,7 @@
+#DESAFIO 049 DO CURSO EM VÍDEO PHYTON
+
+num = int(input('Digite um número e veja sua tabuada: '))
+for vezes in range(1, 11):
+    final = num * vezes
+    print('{} x {} = {}'.format(num, vezes, final))
 
