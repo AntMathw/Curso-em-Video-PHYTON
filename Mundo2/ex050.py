1aed0e61d1898df1286967f63566dc678a1ff5e1
Theuzin <mateusetec424@gmail.com>
2025-05-03 13:22:20 -0300

    Update ex050.py

--- Mundo2/ex050.py
+++ Mundo2/ex050.py
@@ -1,1 +1,10 @@
+#DESAFIO 050 DO CURSO EM VÍDEO PHYTON
 
+soma = 0
+cont = 0
+for c in range(1, 7):
+    numero = int(input('Digite um valor: '))
+    if numero % 2 == 0:
+        soma += numero
+        cont += 1
+print('A soma dos numeros pares é {}, e possui {} pares'.format(soma, cont))
