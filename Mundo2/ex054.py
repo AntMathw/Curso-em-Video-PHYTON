f7993d6e37351d264e3964bb9f66961ace237f30
Theuzin <mateusetec424@gmail.com>
2025-05-05 09:39:16 -0300

    Update ex054.py

--- Mundo2/ex054.py
+++ Mundo2/ex054.py
@@ -1,1 +1,15 @@
+#DESAFIO 054 DO CURSO EM VÍDEO PHYTON
 
+from datetime import date
+
+menor = 0
+maior = 0
+
+for idade in range(0, 7):
+  nascimento = int(input('Digite o ano de nascimento: '))
+    idade2 = date.today().year - nascimento
+    if idade2 >= 18:
+        maior += 1
+    else:
+        menor += 1
+print('De 7 pessoas {} são jovens e {} pessoas são mais velhos'.format(menor, maior))
