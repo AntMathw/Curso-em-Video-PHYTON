e916737d0dfd5a385729aa328e7f4e51ee6cc8cf
Theuzin <mateusetec424@gmail.com>
2025-05-05 09:49:24 -0300

    Update ex055.py

--- Mundo2/ex055.py
+++ Mundo2/ex055.py
@@ -1,1 +1,20 @@
+#DESAFIO 055 DO CURSO EM VÍDEO
+
+menor = 0
+maior = 0
+
+for p in range(1, 6):
+  peso = float(input('Digite o peso da {} pessoa'.format(p)))
+ 
+  if p == 1:
+    peso = maior
+    peso = menor
+  else:
+    if peso > maior:
+      peso = maior
+    if peso < menor:
+      peso = menor
+
+print('Das 5 pessoas, o maior peso é {}KG e o menor peso é {}KG'.format(maior, menor)
+  
 
