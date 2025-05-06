d6635242b6160ae7249f075d00ad52a2d0675e8a
Theuzin <mateusetec424@gmail.com>
2025-05-06 09:35:45 -0300

    Update ex057.py

--- Mundo2/ex057.py
+++ Mundo2/ex057.py
@@ -1,1 +1,7 @@
+#DESAFIO 057 DO CURSO EM VÍDEO PHYTON
+
+sexo = str(input('Digite seu sexo: [M/F]')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados inválidos, por favor informe seu sexo sexo novamente')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
 
