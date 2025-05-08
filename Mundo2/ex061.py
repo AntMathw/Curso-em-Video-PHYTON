d311699db7367238ebb974e6bc59a820738c2162
Theuzin <mateusetec424@gmail.com>
2025-05-08 10:15:43 -0300

    Update ex061.py

--- Mundo2/ex061.py
+++ Mundo2/ex061.py
@@ -1,1 +1,17 @@
+#DESAFIO 061 DO CURSO EM VÍDEO PHYTON
+
+print('Gerador de PA')
+print('=-=' * 6)
+pa = int(input('Primeiro Termo: '))
+r = int(input('Digite a razão: '))
+
+termo = pa
+c = 1
+
+while c <= 10:
+    print('{} => '.format(termo), end=' ')
+    termo += r
+    c += 1
+print('FIM')
+
 
