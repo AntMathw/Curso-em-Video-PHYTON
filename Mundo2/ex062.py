2c6bf4de4d3d02c00561b7b873d65ec8a0c232fd
Theuzin <mateusetec424@gmail.com>
2025-05-08 10:33:19 -0300

    Update ex062.py

--- Mundo2/ex062.py
+++ Mundo2/ex062.py
@@ -1,1 +1,20 @@
+#DESAFIO 062 DO CURSO EM VÍDEO PHYTON
 
+print('Gerador de PA')
+print('=-=' * 6)
+pa = int(input('Primeiro Termo: '))
+r = int(input('A razão: '))
+
+termo = pa
+c = 1
+total = 0
+continuacao = 10
+while continuacao != 0:
+    total = total + continuacao
+    while c <= total:
+        print('{} => '.format(termo), end=' ')
+        termo += r
+        c += 1
+    print('PAUSA')
+    continuacao = int(input('Quantos termos você quer mostrar novamente? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
