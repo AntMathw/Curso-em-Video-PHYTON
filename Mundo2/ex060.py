99b4e54b389a8af4f02db19ed4bdb2b5c745245a
Theuzin <mateusetec424@gmail.com>
2025-05-08 10:04:33 -0300

    Update ex060.py

--- Mundo2/ex060.py
+++ Mundo2/ex060.py
@@ -1,1 +1,15 @@
+#DESAFIO 060 DO CURSO EM VÍDEO PHYTON
+
+fatorial = int(input('Digite um número \nDescubra o seu fatorial: '))
+c = fatorial
+print(f'Calculando {c}! = ', end=' ')
+f = 1
+
+while c > 0:
+    print('{}'.format(c), end=' ')
+    print(' x ' if c > 1 else ' = ', end=' ')
+    f *= c
+    c -= 1
+print('{}'.format(f))
+
 
