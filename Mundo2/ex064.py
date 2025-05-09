583578c3f571ae82a20883a4a8a1cbcfb4125e72
Theuzin <mateusetec424@gmail.com>
2025-05-09 20:45:50 -0300

    Update ex064.py

--- Mundo2/ex064.py
+++ Mundo2/ex064.py
@@ -1,1 +1,11 @@
+#DESAFIO 064 DO CURSO EM VÍDEO PHYTON
+
+num = cont = soma = 0
+num = int(input('Digite um número [999 para parar o programa]: '))
+while num!= 999:
+    soma += num
+    cont += 1
+    num = int(input('Digite um número [999 para parar o programa]: '))
+print('VocÊ digitou tantos números {}, o resultado entre eles é {}'.format(cont, soma))
+print('FIM')
 
