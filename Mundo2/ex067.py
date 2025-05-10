d1b0bfa80227fd093f4d3311b5c6b953a26789bb
Theuzin <mateusetec424@gmail.com>
2025-05-10 15:12:56 -0300

    Update ex067.py

--- Mundo2/ex067.py
+++ Mundo2/ex067.py
@@ -1,1 +1,13 @@
+#DESAFIO 067 DO CURSO EM VÍDEO PHYTON
 
+while True:
+    
+    valor = int(input('Digite um valor e mostre sua tabuada: '))
+    print('=-='*30)
+    if valor < 0:
+        break
+    for c in range(1, 11):
+        print(f'{valor} x {c} = {valor*c}')
+    print('=-='*30)
+
+print('Programa encerrado, você digitou um número negativo')
