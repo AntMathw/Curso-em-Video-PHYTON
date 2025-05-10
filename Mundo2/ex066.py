b9be9bbaa99fa0406b96031803bf5f953be1759a
Theuzin <mateusetec424@gmail.com>
2025-05-10 15:02:42 -0300

    Update ex066.py

--- Mundo2/ex066.py
+++ Mundo2/ex066.py
@@ -1,1 +1,11 @@
+#DESAFIO 066 DO CURSO EM VÍDEO PHYTON
 
+n = cont = soma = 0
+cont = 0
+while True:
+    n = int(input('Digite um valor[999 para parar]: '))
+    if n == 999:
+        break
+    cont += 1
+    soma += n
+print(f'A soma dos {cont} os números é {soma}')
