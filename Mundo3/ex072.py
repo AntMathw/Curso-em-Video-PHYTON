e137fe25aadd996fd0fdace7bf313bf6c78f89ff
Theuzin <mateusetec424@gmail.com>
2025-08-23 15:51:14 -0300

    Update ex072.py

--- Mundo3/ex072.py
+++ Mundo3/ex072.py
@@ -19,9 +19,27 @@ numero = ("zero", "um", "dois", "três", "quatro"
           "dez", "onze", "doze", "treze", "quatorze", ""
           "quinze", "dezesseis", "dezessete", "dezoito", 
           "dezenove", "vinte")
+
 while True:
     if num  <= 20:
         print(f'Você digitou o número {numero[num]}')
         break
     elif num > 20:
         print('Você digitou um número fora da tupla')
+        break
+
+usuário = input('Você quer continuar? [S/N] ')
+if usuário.lower() == 's':
+
+        sim = int(input('Digite um número e vê seu numéro por extenso: '))
+        valores = ("zero", 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 
+                   'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
+                   'dezenove', 'vinte')
+    
+while True:
+        if sim <= 20:
+            print(f'Você digitou o numéro {valores[sim]}')
+            break
+        else:
+            print(f'Você digitou o número fora da tupla {sim}')
+            break
