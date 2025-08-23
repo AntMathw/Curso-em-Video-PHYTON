68d7868fae938efdca71c8565a4d7e5829a96f06
Theuzin <mateusetec424@gmail.com>
2025-08-23 14:38:17 -0300

    Update ex072.py

--- Mundo3/ex072.py
+++ Mundo3/ex072.py
@@ -1,1 +1,27 @@
+#Tuplas número em extenso
 
+num = int(input('Digite um número de 0 a 20: '))
+numero = ("zero", "um", "dois", "três", "quatro"
+          , "cinco", "seis", "sete", "oito", "nove", 
+          "dez", "onze", "doze", "treze", "quatorze", ""
+          "quinze", "dezesseis", "dezessete", "dezoito", 
+          "dezenove", "vinte")
+if num  <= 20:
+    print(f'Você digitou o número {numero[num]}')
+elif num > 20:
+    print('Você digitou um número fora da tupla')
+
+#Segunda forma de número em extenso
+
+num = int(input('Digite um número de 0 a 20: '))
+numero = ("zero", "um", "dois", "três", "quatro"
+          , "cinco", "seis", "sete", "oito", "nove", 
+          "dez", "onze", "doze", "treze", "quatorze", ""
+          "quinze", "dezesseis", "dezessete", "dezoito", 
+          "dezenove", "vinte")
+while True:
+    if num  <= 20:
+        print(f'Você digitou o número {numero[num]}')
+        break
+    elif num > 20:
+        print('Você digitou um número fora da tupla')
